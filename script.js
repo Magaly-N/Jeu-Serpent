6804e19794633ce6932385d241003f68675a4be0
Magaly-N <magalynou@hotmail.fr>
2024-08-01 00:25:19 +0200

    Add method checkCollision

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ window.onload = function () {
     var delay = 700;
     var snakee;
     var applee;
+    var widthInBlocks = canvasWidth / blockSize;
+    var heightInBlocks = canvasHeight / blockSize;
 
     init();
 
@@ -28,11 +30,17 @@ window.onload = function () {
 
     function refreshCanvas() {
 
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
         snakee.advance();
-        snakee.draw();
-        applee.draw();
-        setTimeout(refreshCanvas, delay);
+        if (snakee.checkCollision()) {
+            // GAME OVER
+        }
+        else {
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            snakee.draw();
+            applee.draw();
+            setTimeout(refreshCanvas, delay);
+        }
+
     };
 
     function drawBlock(ctx, position) {
@@ -93,15 +101,44 @@ window.onload = function () {
                 this.direction = newDirection;
             }
         };
+
+        this.checkCollision = function () {
+            var wallCollision = false;
+            var snakeCollision = false;
+            var head = this.body[0];
+            var rest = this.body.slice(1);
+            var snakeX = head[0];
+            var snakeY = head[1];
+            var minX = 0;
+            var minY = 0;
+            var maxX = widthInBlocks - 1;
+            var maxY = heightInBlocks - 1;
+            // Head of snake is outside of the caneva along its length
+            var isNotBetweenHorizontalWalls = snakeX < minX || snakeX > maxX;
+            // Head of snake is outside of the caneva across its width
+            var isNotBetweenVerticalWalls = snakeY < minY || snakeY > maxY;
+
+            if (isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls) {
+                wallCollision = true;
+            };
+
+            for (var i = 0; i < rest.length; i++) {
+                if (snakeX === rest[i][0] && snakeY === rest[i][1]) {
+                    snakeCollision = true;
+                }
+            };
+            return wallCollision || snakeCollision;
+        };
     };
 
     function Apple(position) {
         this.position = position;
         this.draw = function () {
             ctx.save();
-            ctx.fillStyle = "#33cc33";
+            ctx.fillStyle = "#33cc33"; // apple's color
             ctx.beginPath();
             var radius = blockSize / 2;
+            // xCoord and yCoord = middle of the apple
             var x = position[0] * blockSize + radius;
             var y = position[1] * blockSize + radius;
             ctx.arc(x, y, radius, 0, Math.PI * 2, true);
